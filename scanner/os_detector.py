87e027396da592b6466ac3926e8a814be3ba63b9
Edvaldo <edvaldovitor250@gmail.com>
2025-02-22 13:31:56 -0300

    fix: these if things

--- scanner/os_detector.py
+++ scanner/os_detector.py
@@ -1,19 +1,20 @@
+import re
+
 class OSDetector:
     @staticmethod
     def detect(running_str, device_type_str):
-        running_str = running_str.lower()
-        device_type_str = device_type_str.lower()
-        if "windows" in running_str:
-            return "Windows"
-        elif "android" in running_str:
-            return "Android"
-        elif "ios" in running_str or "apple" in running_str:
-            return "iOS"
-        elif "mac os" in running_str:
-            return "MacOS"
-        elif "linux" in running_str:
-            if "phone" in device_type_str:
-                return "Android"
-            return "Linux"
-        else:
-            return "Desconhecido"
+        mapping = {
+            r'windows': 'Windows',
+            r'android': 'Android',
+            r'(ios|apple)': 'iOS',
+            r'mac os': 'MacOS',
+            r'linux': 'Linux'
+        }
+        r_str = running_str.lower()
+        d_str = device_type_str.lower()
+        for pattern, os_name in mapping.items():
+            if re.search(pattern, r_str):
+                if os_name == "Linux" and "phone" in d_str:
+                    return "Android"
+                return os_name
+        return "Desconhecido"
