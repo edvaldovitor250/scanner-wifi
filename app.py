a2293cd8be14310c229650b1719261606db527ac
Edvaldo <edvaldovitor250@gmail.com>
2025-02-22 13:48:55 -0300

    feat: add auth.py in project

--- app.py
+++ app.py
@@ -1,20 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from scanner.network_scanner import NetworkScanner
-from functools import wraps
+from auth import USERNAME, PASSWORD, login_required
 
 app = Flask(__name__)
 app.secret_key = "n3Q,0157}<-"
-
-USERNAME = "admin"
-PASSWORD = "1234"
-
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if not session.get('logged_in'):
-            return redirect(url_for('login'))
-        return f(*args, **kwargs)
-    return decorated_function
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
